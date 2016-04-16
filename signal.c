72f6b129311b90f5482f10d2cf3fad063cad142d
John Cheng <purpon@hotmail.com>
2016-04-16 23:49:01 +0800

    add file

---
+++ signal.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+enum {RED,GREEN,BLUE};
+void printf_color(int);
+void(*Sfunc(int a,void(*fptr)(int b)))(int c);
+
+void print_color(int color)
+{
+	switch (color)
+	{
+		case RED:
+			printf("color is Red\n");
+			break;
+		case GREEN:
+			printf("color is GREEN\n");
+			break;
+		case BLUE:
+			printf("color is Blue\n");
+		break;
+	}
+
+}
+
+void (*Sfunc(int a,void(*fptr)(int b)))(int c)
+{
+	printf("Enter the signal function\n");
+	fptr(a);
+	
+	return fptr;
+}
+
+
+int main()
+{
+	typedef	void(*(*SIGNAL)(int ,void (*)(int)))(int);
+	SIGNAL signal;
+	signal=&Sfunc;
+	Sfunc(RED,print_color)(GREEN);
+	signal(BLUE,print_color)(RED);
+	return 0;
+}
